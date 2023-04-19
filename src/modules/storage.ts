acb39e358fbe13aa74b20ac1b4fd0e3338b2e8e1
Lukas Köhler <lukas.koehler@ueberbit.de>
2023-04-19 19:21:10 +0200

    Create deleteContact function

--- src/modules/storage.ts
+++ src/modules/storage.ts
@@ -25,7 +25,10 @@ This function takes a key in form of a string and deletes the respective Storage
 export function deleteAddressbookByKey(key: string) : void {
     localStorage.removeItem(key);
 }
-
+/*
+Function that creates a contact and writes it to memory. Keep in mind that the contact object has to
+be created prior using the contact interface defined in structs.ts
+ */
 export function createContact(newcontact : Contact, storagekey: string) : void {
     let storage : string | null = localStorage.getItem(storagekey)
     if (storage !== null) {
@@ -35,5 +38,28 @@ export function createContact(newcontact : Contact, storagekey: string) : void {
         let newstorage : string = JSON.stringify(json)
         localStorage.setItem(storagekey, newstorage)
         printJsonAb(storagekey)
+    }
+}
+/*
+The respective function that reads the contacts out of the localStorage and deletes it.
+Uses full dataset comparison to always delete the correct entry without needing to implement IDs.
+ */
+export function deleteContact(contact : Contact, storagekey: string) : void {
+    let storage : string | null = localStorage.getItem(storagekey)
+    if (storage !== null) {
+        let json : any = JSON.parse(storage)
+        for (let i : number = 0; i<json.length; i++) {
+            if (json[i].firstname === contact.firstname && json[i].lastname === contact.lastname && json[i].email === contact.email && json[i].phone === contact.phone) {
+                json.splice(i,i)
+                localStorage.setItem(storagekey,JSON.stringify(json))
+                return
+            }
+        }
+        console.error("There was an error while searching the correct entry to DELETE")
+        return
+    }
+    else {
+        console.error("The requested localStorage Item was not accessible. As a developer you should check the code.")
+        return
     }
 }
